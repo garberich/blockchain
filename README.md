4d01ceb5278e61181af2c272b4cb85cf20aa7bea
Richard Garcia <richardferneygarcia@gmail.com>
2019-01-28 22:05:55 -0500

    Add the proof of work and the Nonce value

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## BLOCKCHAIN APLICATION
 
+This project is usefull for create a personal blockchain application, using hash algoritms, proof of work and the nonce value, etc.
+
+### Reminder
 Download the project and execute ```npm install``` for the dependencies
+
+### For execute the test files
+```
+npm run test
+```
--- blockchain/block.js
+++ blockchain/block.js
@@ -1,11 +1,13 @@
 const SHA256 = require('crypto-js/sha256');
+const { DIFFICULTY } = require('../config');
 
 class Block {
-    constructor(timestamp, lastHash, hash, data) {
+    constructor(timestamp, lastHash, hash, data, nonce) {
         this.timestamp = timestamp;
         this.lastHash = lastHash;
         this.hash = hash;
         this.data = data;
+        this.nonce = nonce;
     }
 
     toString() {
@@ -13,29 +15,44 @@ class Block {
         Timestamp: ${this.timestamp}
         Last Hash: ${this.lastHash.substring(0, 10)}
         Hash     : ${this.hash.substring(0, 10)}
-        Data     : ${this.data}`;
+        Data     : ${this.data}
+        Nonce    : ${this.nonce}`;
     }
 
+    // Bloque Genesis con los valores predeterminados:
+    // Timestamp, ultimo hash, hash, data, nonce
     static genesis() {
-        return new this('Genesis time', '-------', 'f1r57-h45h', [])
+        return new this('Genesis time', '-------', 'f1r57-h45h', [], 0)
     }
 
     static mineBlock(lastBlock, data) {
-        const timestamp = Date.now();
+        let hash, timestamp;
         const lastHash = lastBlock.hash;
-        const hash = Block.hash(timestamp, lastHash, data);
+        let nonce = 0;
 
-        return new this(timestamp, lastHash, hash, data);
+        do {
+            nonce++;
+            timestamp = Date.now();
+            hash = Block.hash(timestamp, lastHash, data, nonce);
+
+            // Evalua si los primeros digitos del hash son el caracter 0
+            // y si la cantidad de esos primeros caracteres es iguales a
+            // la cantidad establecida en la dificultad
+        } while (hash.substring(0, DIFFICULTY) !== '0'.repeat(DIFFICULTY));
+
+        return new this(timestamp, lastHash, hash, data, nonce);
     }
 
-    static hash(timestamp, lastHash, data) {
-        return SHA256(`${timestamp}${lastHash}${data}`).toString();
+    // Funcion para generar el hash encriptado
+    static hash(timestamp, lastHash, data, nonce) {
+        return SHA256(`${timestamp}${lastHash}${data}${nonce}`).toString();
     }
 
+    // Declara las propiedades del bloque y realiza la encriptación
     static blockHash(block) {
-        const { timestamp, lastHash, data } = block;
+        const { timestamp, lastHash, data, nonce } = block;
 
-        return Block.hash(timestamp, lastHash, data);
+        return Block.hash(timestamp, lastHash, data, nonce);
     }
 }
 
